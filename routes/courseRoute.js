f6b02d1d62168092cf3a2eb254f71b9a9d18d0c5
KereBere <alsydm091@gmail.com>
2021-12-12 15:23:19 +0300

    Lesson 06

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -3,7 +3,7 @@ const courseController = require('../controllers/courseController');
 
 const router = express.Router();
 
-router.route('/').post(courseController.createCourse); //* http://localhost: 3000/course requestleri buraya gelir.
-router.route('/').get(courseController.getAllCourses); //* http://localhost: 3000/course requestleri buraya gelir.
+router.route('/').post(courseController.createCourse); //* http://localhost: 3000/courses requestleri buraya gelir.
+router.route('/').get(courseController.getAllCourses); //* http://localhost: 3000/courses requestleri buraya gelir.
 
 module.exports = router;

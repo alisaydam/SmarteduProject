e681096ce249f95b1aee02c17bd5ddda8a9c2e7f
KereBere <alsydm091@gmail.com>
2021-12-15 15:37:20 +0300

    Lesson 13

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.route('/signup').post(authController.createUser); //* http://localhost: 3000/users/signup
 router.route('/login').post(authController.loginUser); //* http://localhost: 3000/users/signup
 router.route('/logout').get(authController.logoutUser); //* http://localhost: 3000/users/signup
+router.route('/dashboard').get(authController.getDashboardPage); //* http://localhost: 3000/users/dashboard
 
 module.exports = router;

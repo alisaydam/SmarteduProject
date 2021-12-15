cfc24573f526b8f7bb117af70fc119e7f3437268
KereBere <alsydm091@gmail.com>
2021-12-15 21:37:21 +0300

    Lesson 18

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,13 +1,16 @@
 const Course = require('../models/Course');
+const User = require('../models/User');
 const Category = require('../models/Category');
 
 exports.createCourse = async (req, res) => {
   try {
-    const course = await Course.create(req.body);
-    res.status(201).json({
-      status: 'Success',
-      course,
+    const course = await Course.create({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      user: req.session.userID,
     });
+    res.status(201).redirect('/courses');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -28,7 +31,7 @@ exports.getAllCourses = async (req, res) => {
       filter = { category: category._id };
     }
 
-    const courses = await Course.find(filter);
+    const courses = await Course.find(filter).sort('-createdAt');
 
     const categories = await Category.find();
 
@@ -47,11 +50,27 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
-    const course = await Course.findOne({ slug: req.params.slug });
+    const course = await Course.findOne({ slug: req.params.slug }).populate(
+      'user'
+    );
     res.status(200).render('course', {
       course,
       page_name: 'courses',
     });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+exports.enrollCourse = async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userID);
+    await user.courses.push({ _id: req.body.course_id });
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.route('/').post(roleMiddlaware(["teacher", "admin"]), courseController.createCourse); //* http://localhost: 3000/courses requestleri buraya gelir.
 router.route('/').get(courseController.getAllCourses); //* http://localhost: 3000/courses requestleri buraya gelir.
 router.route('/:slug').get(courseController.getCourse); //* http://localhost: 3000/courses/id requestleri buraya gelir.
+router.route('/enroll').post(courseController.enrollCourse); //* http://localhost: 3000/courses requestleri buraya gelir.
+
 
 module.exports = router;

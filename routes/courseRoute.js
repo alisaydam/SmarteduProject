5acc147a9187a78fe654b5abc9e97fc12992b2dd
KereBere <alsydm091@gmail.com>
2021-12-15 17:10:33 +0300

    Lesson 15

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const courseController = require('../controllers/courseController');
+const roleMiddlaware = require('../middlewares/roleMiddleware');
 
 const router = express.Router();
 
-router.route('/').post(courseController.createCourse); //* http://localhost: 3000/courses requestleri buraya gelir.
+router.route('/').post(roleMiddlaware(["teacher", "admin"]), courseController.createCourse); //* http://localhost: 3000/courses requestleri buraya gelir.
 router.route('/').get(courseController.getAllCourses); //* http://localhost: 3000/courses requestleri buraya gelir.
 router.route('/:slug').get(courseController.getCourse); //* http://localhost: 3000/courses/id requestleri buraya gelir.
 

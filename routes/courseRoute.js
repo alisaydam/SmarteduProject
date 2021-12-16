4a385f872cb56caaea8bfc0d5ca0a5097de2ce2a
KereBere <alsydm091@gmail.com>
2021-12-16 22:53:30 +0300

    Lesson 25

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -7,6 +7,8 @@ const router = express.Router();
 router.route('/').post(roleMiddlaware(["teacher", "admin"]), courseController.createCourse); //* http://localhost: 3000/courses requestleri buraya gelir.
 router.route('/').get(courseController.getAllCourses); //* http://localhost: 3000/courses requestleri buraya gelir.
 router.route('/:slug').get(courseController.getCourse); //* http://localhost: 3000/courses/id requestleri buraya gelir.
+router.route('/:slug').delete(courseController.deleteCourse); //* http://localhost: 3000/courses/id requestleri buraya gelir.
+router.route('/:slug').put(courseController.updateCourse); //* http://localhost: 3000/courses/id requestleri buraya gelir.
 router.route('/enroll').post(courseController.enrollCourse); //* http://localhost: 3000/courses requestleri buraya gelir.
 router.route('/release').post(courseController.releaselCourse); //* http://localhost: 3000/courses requestleri buraya gelir.
 
